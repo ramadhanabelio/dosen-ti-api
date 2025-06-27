34c7265445ada82b98094c545f21a44d4b8c4c05
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2025-06-28 02:07:03 +0700

    fix: update default image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                 <li class="nav-item dropdown pe-3">
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                         data-bs-toggle="dropdown">
-                        <img src="{{ asset('img/logo.png') }}" alt="Profile" class="rounded-circle" />
+                        <img src="{{ asset('img/default.png') }}" alt="Profile" class="rounded-circle" />
                         <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
                     </a>
                     <!-- End Profile Image Icon -->
