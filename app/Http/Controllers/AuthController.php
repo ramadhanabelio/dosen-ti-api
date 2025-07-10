ccc83858dfa20c5aa1843b1069233fec81bee1f1
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2025-07-10 19:49:39 +0700

    fix: auth guard web

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
             'password' => 'required|string',
         ]);
 
-        if (Auth::attempt($credentials, $request->filled('remember'))) {
+        if (Auth::guard('web')->attempt($credentials, $request->filled('remember'))) {
             $request->session()->regenerate();
 
             return redirect()->route('dashboard');
@@ -34,7 +34,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        Auth::logout();
+        Auth::guard('web')->logout();
 
         $request->session()->invalidate();
         $request->session()->regenerateToken();
